76bea9498a7c0edeb3fa8aa9ffc85758114386ac
lyndallh <Lindi.harding@gmail.com>
2024-02-16 11:00:17 +0000

    Nav bar basic list styling

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,13 +1,20 @@
-import {Link,Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
+import "./NavBar.css";
 
 function NavBar() {
   return (
     <div>
-        <nav>
-          <Link to="/">Home</Link>
-          <Link to="/about">About</Link>
-    </nav>
-    <Outlet />
+      <nav className="navbar">
+        <ul className="navbar-ul">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/projects">Projects</Link>
+          </li>
+        </ul>
+      </nav>
+      <Outlet />
     </div>
   );
 }
