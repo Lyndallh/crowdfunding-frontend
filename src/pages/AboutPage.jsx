820476acc2723c48878a5a312be5461301f6756c
lindi <lindi.harding@gmail.com>
2024-02-21 11:36:33 +0800

    new pages included

---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,11 @@
+function AboutPage() {
+    return(
+        <div>
+            <h2>About Us</h2>
+            <p>Our mission is to help you fund your dreams</p>
+
+        </div>
+    );
+}
+
+export default AboutPage;
---
+++ src/pages/ContactPage.jsx
@@ -0,0 +1,12 @@
+function ContactPage() {
+  return (
+    <div>
+      <h1>Contact Us</h1>
+      <p>
+        You can contact us via email at{" "}
+        <a href="mailto: l@l.com"></a>
+        </p>    
+    </div>
+    );
+}
+export default ContactPage;
