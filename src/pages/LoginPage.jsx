483c2710c971e7d6d47ed5518ed35f4e7541422c
lindi <lindi.harding@gmail.com>
2024-02-25 13:25:30 +0800

    adding link to signuppage to Login page

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,6 +1,17 @@
 import LoginForm from "../components/LoginForm";
+import { Link } from "react-router-dom";
 
 function LoginPage(){
-    return <LoginForm />;
+    return (
+    <div>
+        <LoginForm />
+        OR 
+        <p>
+        <Link to="/signup">Sign Up Here</Link>
+        </p>
+
+    </div>
+    )
+    ;
 }
 export default LoginPage;
